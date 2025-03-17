3eacd4c70fc5dafe88adda0f4886b61a587ca319
ccarnell <23458960+ccarnell@users.noreply.github.com>
2025-03-17 14:59:55 -0500

    Update v2 content

--- app/data.ts
+++ app/data.ts
@@ -34,29 +34,27 @@ type SocialLink = {
 
 export const PROJECTS: Project[] = [
   {
+    name: 'Version 2 (current version)',
+    description:
+      'Deployed new Vercel boilerplate. Began working with Next.js, Typescript, MongoDB, and setup Claude Code via Anthropic API. Gave it some fancy clothes before submitting my resume.',
+    link: 'https://chrisbuildswithclaude.com',
+    video: '/v2-chrisbuildswithclaude-project-image.webp',
+    id: 'project2',
+  },
+  {
     name: 'Version 1',
     description:
-      'Simply deployed my first Next.js app via Vercel to hosted domain. Moving on to v2 quickly.',
-    link: 'https://www.v1.chrisbuildswithclaude.com',
+      'Simply deployed my first Next.js app via Vercel to hosted domain purchased through Cloudflare to learn programming environment setup and GitHub workflow. Moving on to v2 quickly.',
+    link: 'https://v1.chrisbuildswithclaude.com',
     video: '/v1-chrisbuildswithclaude-project-image.png',
     id: 'project1',
   },
-  // Comment out the second project until we have content for it
-  /*
-  {
-    name: 'Name_WIP2',
-    description: 'description_wip2',
-    link: 'url of Project 2 title',
-    video: 'image/video of Project 2',
-    id: 'project2',
-  },
-  */
 ]
 
 export const SNAPSHOT: Snapshot[] = [
   {
-    title: "Backlog: Ideating Update 11",
-    description: "The <strong>Deployment Objective</strong> I set out to do was completed on the first day. I then spent some time randomly working through different additional technical functionality for self-interest and to continue showing technical learning. Since I have applied for the Strategic Product Management position, I am now going to take a step back from the demonstration of quickly learning technical implementation details to showcase more intentional and holistic product management capabilities.\n\nAfter discussing the importance of AI safety and ethics I will showcase how I actually go about strategic product development.\n1.) Within v2, display past work, tools, and frameworks used\n2.) v3. I will then showcase actually managing the strategy of a real product for Anthropic. With my background, I will focus on the Economic Index and display systematically going through the process as if I were working within a team collaborating.",
+    title: "Backlog: Ideate Update 11",
+    description: "The initial <strong>Idea Concept</strong> to purchase the domain and learn to setup a local environment, Vercel, and GitHub workflow to deploy was completed on the first day. I then spent some time working through various additional technical functionality for self-interest and to continue showing technical learning.\n\nSince I have applied for the Strategic Product Management position, I am now going to take a step back from the demonstration of quickly learning technical implementation details to showcase more holistic and intentional strategic product management capabilities.\n\nAfter discussing the importance of AI safety and ethics I will:\n1.) Within v2, display past work, tools, and frameworks used for strategic product management\n2.) Begin v3. A prototype. I will showcase managing the strategy of a real product for Anthropic. With my background, I will focus on Anthropic's Economic Index and display systematically going through the process as if I were working within a collaborative team.",
     id: "snapshot11",
   },
   {
@@ -65,13 +63,13 @@ export const SNAPSHOT: Snapshot[] = [
     id: "snapshot10",
   },
   {
-    title: "Update 9: Add Previous Work section",
-    description: "Continued to work with Claude Code. Added a few startups and products that I have previously either helped build from scratch or worked directly with technical and non-technical founders. Added Vercel Analytics so we have measurements in place as a tool to identify if the product is successful in its objective. Added an Expand/Collapse functionality with a preview display directly above, and dressed up some of the site's styling.",
+    title: "Update 9: Add new component, animation, and analytics",
+    description: "Continued to work with Claude Code. Added Previous Work section with a few products and startup founders I've worked with.",
     id: "snapshot9",
   },
   {
-    title: "Update 8: Update styling and better component comprehension",
-    description: "Updated motion-primitive animations under the Selected Projects section to accept image and/or video formats. Added title, image, link, and description to v1 site. Updated phone number in Header to include sms: and tel: icons and functionality. Threw in some Anthropic accent colors but didn't change the entire color palette after discussing with Claude that it would probably raise some ethical concerns with trademark/brand identity and potential confusion. Completely understandable",
+    title: "Update 8: Update Typescript files and styling",
+    description: "Updated motion-primitive animations and other styling in Typescript files. Added Anthropic accent colors, but didn't change the entire color palette after discussing with Claude that it would probably raise some ethical concerns with trademark/brand identity and potential confusion. Completely understandable",
     id: "snapshot8",
   },
   {
@@ -81,11 +79,11 @@ export const SNAPSHOT: Snapshot[] = [
   },
   {
     title: "Update 6: When to take a breather",
-    description: "Ran into some npm Git, Next.js, command-line and environment file issues while trying to setup MongoDB with a v3. Since a database isn't critical right now, decided to abandon and overwrite v2. Wanted at least a small win, so completed some easier tasks with updating favicon, and hosting SVG icons while custom developing an email sig with HTML",
+    description: "Ran into some npm Git, Next.js, command-line and environment file issues while trying to setup MongoDB with a v3. A database isn't critical right now—decided to overwrite v2. Wanted at least a small win—completed some easier tasks: updated favicon, and created custom HTML email sig using /public to host SVG icons",
     id: "snapshot6",
   },
   {
-    title: "Update 5: Folders files, and functions",
+    title: "Update 5: Folders, files, and functions",
     description: "Reviewed pages, public, styles, components, package.json and navigated making updates to mainly Typescript files. Learning more about Next.js and Tailwind. Changed some initial text from boilerplate. Added Anthropic's mission to footer",
     id: "snapshot5",
   },
@@ -95,17 +93,17 @@ export const SNAPSHOT: Snapshot[] = [
   id: "snapshot4",
   },
   {
-    title: "Update 3: Git workflow",
+    title: "Update 3: Git workflows",
     description: "Moved static site to new v1 Vercel project and created v1 branch in GitHub to keep versioning records. Kept main branch for most updated project. Learned clone, add, commmit, push flow for v2 on Vercel and GitHub",
     id: "snapshot3",
   },
   {
-    title: "Update 2: Email forwarding",
+    title: "Update 2: Email forward",
     description: "Set ImprovMX & added SPF records in GoDaddy to setup Gmail's SMTP for personal domain",
     id: "snapshot2",
   },  
   {
-    title: "Update 1: Setup, local deployment, and hosting",
+    title: "Update 1: Environment setup, local host, and deploy",
     description: "Setup domain via Cloudflare, deployed a boilerplate static project via Vercel, updated DNS and verified SSL. Setup GitHub, installed Git, node.js, etc, and setup VS Code. Learned to deploy and test localhost",
     id: "snapshot1",
   },
@@ -122,7 +120,7 @@ export const PREVIOUS_WORK: PreviousWork[] = [
   {
     id: "previouswork2",
     title: "PubGen.AI",
-    description: "Multi-modal AI-powered newsroom focused on revitalizaing newsroom deserts",
+    description: "Multi-modal AI-powered platform focused on revitalizaing newsroom deserts across the U.S.",
     imageUrl: "/pubgen-ai-previous-work-screenshot.webp",
     externalUrl: "https://www.pubgen.ai/"
   },
@@ -136,14 +134,14 @@ export const PREVIOUS_WORK: PreviousWork[] = [
   {
     id: "previouswork4",
     title: "Sho.AI",
-    description: "Brand-focused AI platform for companies to manage entire brand systems",
+    description: "Design-focused AI platform for companies to manage entire brand systems",
     imageUrl: "/sho-ai-previous-work-screenshot.webp",
     externalUrl: "https://www.sho.ai/"
   },
   {
     id: "previouswork5",
     title: "Swipesum",
-    description: "Over $30B in OCR payment processing from fortune 500 companies to small businesses",
+    description: "OCR payment processing from fortune 500 companies to small businesses",
     imageUrl: "/swipesum-previous-work-screenshot.webp",
     externalUrl: "https://www.swipesum.com/"
   },
